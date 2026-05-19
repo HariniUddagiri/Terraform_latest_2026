7f6822d68f6694f5a6590e534d5edb37f8c4cbb9
unknown <HariniUddagiri>
2026-05-19 23:41:46 +0530

    Added

--- ec2.tf
+++ ec2.tf
@@ -2,18 +2,10 @@ resource "aws_security_group" "example" {
   name        = "example-security-group"
   description = "Example security group"
 
+     egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-    from_port   = 0     
-    to_port=0
-    protocol="-1"
-    cidr_blocks=["0.0.0.0/0"]
-    }
